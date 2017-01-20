36d1414393abee43fad332fb7d4d2647955a1c2b
avielkar <avraham.elkar@gmail.com>
2017-01-20 21:52:47 +0200

    Showing the variables to the gui is done.
    Now need to save the variables from the gui if were changed into the
    variables dictionay in order to keep changes with the user gui iinputs.

--- code/PinkyAndBrain/PinkyAndBrain/GuiInterface.cs
+++ code/PinkyAndBrain/PinkyAndBrain/GuiInterface.cs
@@ -26,9 +26,16 @@ namespace PinkyAndBrain
         /// </summary>
         private Variables _variablesList;
 
+        /// <summary>
+        /// The excel loader for reading data configuration.
+        /// </summary>
         private ExcelProtocolConfigFieLoader _excelLoader;
 
-
+        /// <summary>
+        /// The list of the dynamic allocated textboxes that allocated each time the user choose different protocol.
+        /// It saves the dynamic TextBox reference.
+        /// </summary>
+        private List<Control> _dynamicAllocatedTextBoxes;
 
         /// <summary>
         /// Constructor.
@@ -39,7 +46,7 @@ namespace PinkyAndBrain
             _excelLoader = excelLoader;
             _variablesList = new Variables();
             _variablesList._variablesDictionary = new Dictionary<string, Variable>();
-
+            _dynamicAllocatedTextBoxes = new List<Control>();
         }
 
         /// <summary>
@@ -117,29 +124,36 @@ namespace PinkyAndBrain
             int top = 100;
             int left = 10;
             int z = 0;
+
+            //clear dynamic textboxes and labels from the last uploaded protocol before creating the new dynamic controls..
+            ClearDynamicControls();
+
             foreach (string varName in _variablesList._variablesDictionary.Keys)
             {
                 Label newLabel = new Label();
                 this.Controls.Add(newLabel);
                 newLabel.Name = _variablesList._variablesDictionary[varName]._description["nice_name"]._ratHouseParameter[0];
                 newLabel.Text = _variablesList._variablesDictionary[varName]._description["nice_name"]._ratHouseParameter[0];
-                newLabel.Width = 200;
+                newLabel.Width = 150;
                 newLabel.Height = 14;
                 newLabel.Top = top;
                 newLabel.Left = left;
 
-                ShowVariableAttributes(varName, top, left, 200, 14);
+                ShowVariableAttributes(varName, top, left, 250, 14 , 230 , 1100);
 
                 top += 35;
             }
         }
 
-        private void ShowVariableAttributes(string varName , int top , int left , int width  , int height)
+        private void ShowVariableAttributes(string varName , int top , int left , int width  , int height , int eachDistance , int offset)
         {
-            #region status ComboBox
+            //string builder for making the text to show to the gui.
+            StringBuilder sBuilder = new StringBuilder();
+
+            #region STATUS_COMBOBOX
             //add the status ComboBox.
             ComboBox statusCombo = new ComboBox();
-            statusCombo.Left = left + 1000;
+            statusCombo.Left = left + offset;
             statusCombo.Top = top;
             statusCombo.Items.Add("Static");
             statusCombo.Items.Add("Varying");
@@ -168,29 +182,217 @@ namespace PinkyAndBrain
 
             //add the status ComboBox to the gui.
             this.Controls.Add(statusCombo);
-            #endregion
+            _dynamicAllocatedTextBoxes.Add(statusCombo);
+            #endregion STATUS_COMBOBOX
 
+            offset -= eachDistance;
+
+            #region INCREMENT_TEXTBOX
+            //add the low bound textbox.
+            TextBox incrementTextBox = new TextBox();
+            incrementTextBox.Left = offset;
+            incrementTextBox.Top = top;
+
+            //check if need to show two parameters of the _landscapeParameters and _ratHouseParameter or only the _ratHouseParameter.
+            //show both parameters.
+            if (_variablesList._variablesDictionary[varName]._description["increament"]._bothParam)
+            {
+                string incrementTextVala = string.Join(",", _variablesList._variablesDictionary[varName]._description["increament"]._ratHouseParameter);
+                string incrementTextValb = string.Join(",", _variablesList._variablesDictionary[varName]._description["increament"]._landscapeParameters);
+
+                incrementTextBox.Text = BracketsAppender(sBuilder, incrementTextVala, incrementTextValb);
+            }
+
+            //show only the _ratHouseParameter.
+            else
+            {
+                string lowBoundTextVal = string.Join(",", _variablesList._variablesDictionary[varName]._description["increament"]._ratHouseParameter);
+                incrementTextBox.Text = lowBoundTextVal;
+            }
+
+            this.Controls.Add(incrementTextBox);
+            _dynamicAllocatedTextBoxes.Add(incrementTextBox);
+            #endregion INCREMENT_TEXTBOX
+
+            offset -= eachDistance;
+
+            #region HIGHBOUND_TEXTBOX
+            //add the low bound textbox.
+            TextBox highBoundTextBox = new TextBox();
+            highBoundTextBox.Left = offset;
+            highBoundTextBox.Top = top;
+
+            //check if need to show two parameters of the _landscapeParameters and _ratHouseParameter or only the _ratHouseParameter.
+            //show both parameters.
+            if (_variablesList._variablesDictionary[varName]._description["high_bound"]._bothParam)
+            {
+                string highBoundTextVala = string.Join(",", _variablesList._variablesDictionary[varName]._description["high_bound"]._ratHouseParameter);
+                string highBoundTextValb = string.Join(",", _variablesList._variablesDictionary[varName]._description["high_bound"]._landscapeParameters);
+
+                highBoundTextBox.Text = BracketsAppender(sBuilder, highBoundTextVala, highBoundTextValb);
+            }
+
+            //show only the _ratHouseParameter.
+            else
+            {
+                string highBoundTextVal = string.Join(",", _variablesList._variablesDictionary[varName]._description["high_bound"]._ratHouseParameter);
+                highBoundTextBox.Text = highBoundTextVal;
+            }
+
+            this.Controls.Add(highBoundTextBox);
+            this._dynamicAllocatedTextBoxes.Add(highBoundTextBox);
+            #endregion HIGHBOUND_TEXTBOX
+
+            offset -= eachDistance;
+
+            #region LOWBOUND_TEXTBOX
             //add the low bound textbox.
             TextBox lowBoundTextBox = new TextBox();
-            lowBoundTextBox.Left = left + 800;
+            lowBoundTextBox.Left = offset;
             lowBoundTextBox.Top = top;
 
             //check if need to show two parameters of the _landscapeParameters and _ratHouseParameter or only the _ratHouseParameter.
             //show both parameters.
             if(_variablesList._variablesDictionary[varName]._description["low_bound"]._bothParam)
             {
+                string lowBoundTextVala = string.Join(",", _variablesList._variablesDictionary[varName]._description["low_bound"]._ratHouseParameter);
+                string lowBoundTextValb = string.Join(",", _variablesList._variablesDictionary[varName]._description["low_bound"]._landscapeParameters);
+
+                lowBoundTextBox.Text = BracketsAppender(sBuilder, lowBoundTextVala, lowBoundTextValb);
             }
 
             //show only the _ratHouseParameter.
             else
             {
-                string lowBoundTextVal = string.Join(" " , _variablesList._variablesDictionary[varName]._description["low_bound"]._ratHouseParameter);
+                string lowBoundTextVal = string.Join("," , _variablesList._variablesDictionary[varName]._description["low_bound"]._ratHouseParameter);
                 lowBoundTextBox.Text = lowBoundTextVal;
             }
 
             this.Controls.Add(lowBoundTextBox);
+            _dynamicAllocatedTextBoxes.Add(lowBoundTextBox);
+            #endregion LOWBOUND_TEXTBOX
 
+            offset -= eachDistance;
+
+            #region PARMETERS_VALUE_TEXTBOX
+            //add the low bound textbox.
+            TextBox parametersTextBox = new TextBox();
+            parametersTextBox.Left = offset;
+            parametersTextBox.Top = top;
+
+            //print the parameter in the gui according to the representation of each status.
+            switch (_variablesList._variablesDictionary[varName]._description["status"]._ratHouseParameter[0])
+            {
+                case "1":   //static
+                    //check if need to show two parameters of the _landscapeParameters and _ratHouseParameter or only the _ratHouseParameter.
+                    //show both parameters.
+                    if (_variablesList._variablesDictionary[varName]._description["parameters"]._bothParam)
+                    {
+                        string parametersTextBoxa = string.Join(",", _variablesList._variablesDictionary[varName]._description["parameters"]._ratHouseParameter);
+                        string parametersTextBoxb = string.Join(",", _variablesList._variablesDictionary[varName]._description["parameters"]._landscapeParameters);
+
+                        parametersTextBox.Text = BracketsAppender(sBuilder, parametersTextBoxa, parametersTextBoxb);
+                    }
+
+                    //show only the _ratHouseParameter.
+                    else
+                    {
+                        string parametersTextVal = string.Join(",", _variablesList._variablesDictionary[varName]._description["parameters"]._ratHouseParameter);
+                        parametersTextBox.Text = parametersTextVal;
+                    }
+                    break;
+                    
+                case "2":   //varying
+                case "3":   //acrossstair
+                case "4":   //withinstair
+                    if (_variablesList._variablesDictionary[varName]._description["parameters"]._bothParam)
+                    {
+                        string lowboundTextVala = string.Join(",", _variablesList._variablesDictionary[varName]._description["low_bound"]._ratHouseParameter);
+                        string highboundTextVala = string.Join(",", _variablesList._variablesDictionary[varName]._description["high_bound"]._ratHouseParameter);
+                        string increasingTextVala = string.Join(",", _variablesList._variablesDictionary[varName]._description["increament"]._ratHouseParameter);
+
+                        string lowboundTextValb = string.Join(",", _variablesList._variablesDictionary[varName]._description["low_bound"]._landscapeParameters);
+                        string highboundTextValb = string.Join(",", _variablesList._variablesDictionary[varName]._description["high_bound"]._landscapeParameters);
+                        string increasingTextValb = string.Join(",", _variablesList._variablesDictionary[varName]._description["increament"]._landscapeParameters);
+
+                        string partA = ThreeStagesRepresentation(sBuilder, lowboundTextVala, increasingTextVala, highboundTextVala);
+                        string partB = ThreeStagesRepresentation(sBuilder, lowboundTextValb, increasingTextValb, highboundTextValb);
+
+                        parametersTextBox.Text = BracketsAppender(sBuilder , partA, partB);
+                    }
+
+                    //show only the _ratHouseParameter.
+                    else
+                    {
+                        string lowboundTextVal = string.Join(",", _variablesList._variablesDictionary[varName]._description["low_bound"]._ratHouseParameter);
+                        string highboundTextVal = string.Join(",", _variablesList._variablesDictionary[varName]._description["high_bound"]._ratHouseParameter);
+                        string increasingTextVal = string.Join(",", _variablesList._variablesDictionary[varName]._description["increament"]._ratHouseParameter);
+
+                        parametersTextBox.Text = ThreeStagesRepresentation(sBuilder, lowboundTextVal, increasingTextVal, highboundTextVal);
+                    }
+                    break;
+
+            }
+
+            this.Controls.Add(parametersTextBox);
+            _dynamicAllocatedTextBoxes.Add(parametersTextBox);
+            #endregion PARMETERS_VALUE_TEXTBOX
+
+            offset -= eachDistance;
         }
+
+        /// <summary>
+        /// Clears the dynamic allocated textboxes and labels due to the previous loaded protocol..
+        /// </summary>
+        private void ClearDynamicControls()
+        {
+            foreach (Control ctrl in _dynamicAllocatedTextBoxes)
+            {
+                this.Controls.Remove(ctrl);
+            }
+
+            _dynamicAllocatedTextBoxes.RemoveAll(x=> true);
+        }
+
+        /// <summary>
+        /// Add manipulation to the partA and partB for showing them in the gui as in the excel file.
+        /// </summary>
+        /// <param name="sBuilder">The stream builder to buld the string with.</param>
+        /// <param name="partA">The first part to put to the string.</param>
+        /// <param name="partB">The second part to put to the string.</param>
+        /// <returns>[partA][partB]</returns>
+        private string BracketsAppender(StringBuilder sBuilder , string partA , string partB)
+        {
+            sBuilder.Clear();
+            sBuilder.Append("[");
+            sBuilder.Append(partA);
+            sBuilder.Append("][");
+            sBuilder.Append(partB);
+            sBuilder.Append("]");
+
+            return sBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Creates a string for the gui with 3 steps.
+        /// </summary>
+        /// <param name="sBuilder">The string writer.</param>
+        /// <param name="lowPart">The low bound.</param>
+        /// <param name="increasingPart">Increament.</param>
+        /// <param name="highPart">The high bound.</param>
+        /// <returns>The string to the gui by [low:inc:high].</returns>
+        private string ThreeStagesRepresentation(StringBuilder sBuilder , string lowPart , string increasingPart , string highPart)
+        {
+            sBuilder.Clear();
+            sBuilder.Append(lowPart);
+            sBuilder.Append(":");
+            sBuilder.Append(increasingPart);
+            sBuilder.Append(":");
+            sBuilder.Append(highPart);
+            return sBuilder.ToString();
+        }
+
+
 
         #endregion
 
