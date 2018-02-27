d60ef0c19c909ba439777fa796138c8fdb9231ca
avielkar <avraham.elkar@gmail.com>
2018-02-27 16:46:01 +0200

    Right & Left Equals checkBox deals with the cohernce left and right parameter also.

--- code/PinkyAndBrain/PinkyAndBrain/GuiInterface.cs
+++ code/PinkyAndBrain/PinkyAndBrain/GuiInterface.cs
@@ -1925,6 +1925,7 @@ namespace PinkyAndBrain
             UpdateRightCheckBoxAvailability("REWARD_RIGHT_DURATION", "REWARD_LEFT_DURATION", equals);
             UpdateRightCheckBoxAvailability("REWARD_RIGHT_DELAY_SC", "REWARD_LEFT_DELAY_SC", equals);
             UpdateRightCheckBoxAvailability("REWARD_RIGHT_DURATION_SC", "REWARD_LEFT_DURATION_SC", equals);
+            UpdateRightCheckBoxAvailability("COHERENCE_RIGHT_STRIP", "COHERENCE_LEFT_STRIP", equals);
         }
 
         /// <summary>
