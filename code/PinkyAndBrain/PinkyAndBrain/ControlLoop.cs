5860feb7cdea79337f44089957d4c88ff6f69086
avielkar <avraham.elkar@gmail.com>
2018-05-24 23:50:22 +0300

    Fix the problem also with the second response time stage 1000 multiplication.

--- code/PinkyAndBrain/PinkyAndBrain/ControlLoop.cs
+++ code/PinkyAndBrain/PinkyAndBrain/ControlLoop.cs
@@ -1046,7 +1046,7 @@ namespace PinkyAndBrain
             sw.Start();
 
             //time to wait for the moving rat response. if decided about a side so break and return the decision and update the _totalCorrectAnsers.
-            while (sw.ElapsedMilliseconds < 1000 * (int)(_currentTrialTimings.wResponseTime))
+            while (sw.ElapsedMilliseconds < (int)(1000 * _currentTrialTimings.wResponseTime))
             {
                 if (_currentRatResponse == (byte)_correctDecision)
                 {
