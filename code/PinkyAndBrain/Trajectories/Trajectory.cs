23a10222ab708265401c9070c121ed22d1731f0f
avielkar <avraham.elkar@gmail.com>
2018-10-05 10:42:33 +0300

    Adding the Position class to seperate file.

--- code/PinkyAndBrain/Trajectories/Trajectory.cs
+++ code/PinkyAndBrain/Trajectories/Trajectory.cs
@@ -227,39 +227,4 @@ namespace Trajectories
             return inverseTrajectory;
         }
     }
-
-    public class Position
-    {
-        #region LINEAR_TRAJECTORY
-        /// <summary>
-        /// The x axis for the position.
-        /// </summary>
-        public double X { get; set; }
-
-        /// <summary>
-        /// The y axis for the position.
-        /// </summary>
-        public double Y { get; set; }
-
-        /// <summary>
-        /// The z axis for the position.
-        /// </summary>
-        public double Z { get; set; }
-        #endregion LINEAR_TRAJECTORY
-
-        #region ROTATION_TRAJECTORY
-        /// <summary>
-        /// The x rotation axis for the position.
-        /// </summary>
-        public double RX { get; set; }
-        /// <summary>
-        /// The y rotation axis for the position.
-        /// </summary>
-        public double RY { get; set; }
-        /// <summary>
-        /// The z rotation axis for the position.
-        /// </summary>
-        public double RZ { get; set; }
-        #endregion ROTATION_TRAJECTORY
-    }
 }
